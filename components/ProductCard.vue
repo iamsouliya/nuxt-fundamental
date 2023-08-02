11a6435f53b461208838ec10f0bd551da456113b
iamsouliya <souliyatmv24@gmail.com>
2023-08-02 16:09:09 +0700

    update dependency

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -3,15 +3,15 @@
     <v-card variant="elevated" class="mx-auto" :border="false" max-width="400">
       <v-card-item>
         <v-card-title>
-          <v-img height="150" :src="product.image" class="text-white" />
+          <v-img height="150" :src="product?.image" class="text-white" />
         </v-card-title>
       </v-card-item>
       <v-card-text>
-        <p class="truncate font-sans">{{ product.title }}</p>
+        <p class="truncate font-sans">{{ product?.title }}</p>
       </v-card-text>
       <v-card-actions>
         <v-btn size="small" rounded variant="flat" color="green">
-          <NuxtLink :to="`/products/${product.id}`" class="font-sans truncate">
+          <NuxtLink :to="`/products/${product?.id}`" class="font-sans truncate">
             SEE MORE DETAILS
           </NuxtLink>
         </v-btn>
--- components/ProductDetail.vue
+++ components/ProductDetail.vue
@@ -2,15 +2,15 @@
   <div class="card">
     <main class="grid grid-cols-2 gap-10">
       <article class="p-7">
-        <img :src="product.image" alt="product image" class="mx-auto my-7" />
+        <img :src="product?.image" alt="product image" class="mx-auto my-7" />
       </article>
       <article class="p-7">
         <h2 class="text-4xl my-7">
-          {{ product.title }}
+          {{ product?.title }}
         </h2>
-        <p class="text-xl my-7">Price - ${{ product.price }}</p>
+        <p class="text-xl my-7">Price - ${{ product?.price }}</p>
         <h3 class="font-bold border-b-2 mb-4 pb-2">Product description:</h3>
-        <p class="mb-7">{{ product.description }}</p>
+        <p class="mb-7">{{ product?.description }}</p>
         <button class="btn flex">
           <i class="material-icons mr-2">add_shopping_cart</i>
           <span>Add to cart</span>
